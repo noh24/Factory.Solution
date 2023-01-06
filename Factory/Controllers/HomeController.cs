62344248734d626a1f9b575964aa66be124fc06f
Brian Noh <noh24@ymail.com>
2023-01-06 13:24:13 -0500

    add working splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Factory.Controllers
 {
@@ -16,7 +18,10 @@ namespace Factory.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      List<Location> model = _db.Locations.ToList();
+      List<Location> model = _db.Locations
+        .Include(location => location.Engineers)
+        .Include(location => location.Machines)
+        .ToList();
       return View(model);
     }
   }
