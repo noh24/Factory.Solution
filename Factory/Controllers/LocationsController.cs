c5b931d3709cc01f2c3f2f5d3bfdc5a8df11873e
Brian Noh <noh24@ymail.com>
2023-01-06 13:24:30 -0500

    add location controller & view

---
+++ Factory/Controllers/LocationsController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Factory.Models;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Controllers
+{
+  public class LocationsController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public LocationsController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Location> model = _db.Locations.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Location location)
+    {
+      _db.Locations.Add(location);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Location thisLocation = _db.Locations
+        .Include(location => location.Engineers)
+        .Include(location => location.Machines)
+        .FirstOrDefault(location => location.LocationId == id);
+      return View(thisLocation);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Location thisLocation = _db.Locations.FirstOrDefault(location => location.LocationId == id);
+      return View(thisLocation);
+    }
+    [HttpPost]
+    public ActionResult Edit(Location location)
+    {
+      _db.Locations.Update(location);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = location.LocationId});
+    }
+    [HttpPost]
+    public ActionResult Delete(int id)
+    {
+      Location thisLocation = _db.Locations.FirstOrDefault(location => location.LocationId == id);
+      _db.Locations.Remove(thisLocation);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
